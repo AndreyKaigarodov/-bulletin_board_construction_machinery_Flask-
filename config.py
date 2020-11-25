a25b24a9fef633ee57339b85a478d89f9d31941c
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2020-11-26 00:36:05 +0500

    add sqlAlchemy

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 dbconfig  = {
     'host':'127.0.0.1',
     'user':'build_tech',
@@ -7,5 +8,6 @@ dbconfig  = {
 }
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY1') or 'qweqeqweqe'
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://build_tech:19982012@localhost/find_build_techdb"
     
 #///////
